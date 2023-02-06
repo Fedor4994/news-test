fc980653637fe2867a732dc3c4bea6622e2c86d5
Fedor4994 <103943126+Fedor4994@users.noreply.github.com>
2023-02-06 21:00:05 +0200

    FIll input value after go back

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ManageSearchIcon from "@mui/icons-material/ManageSearch";
 import { InputAdornment, TextField } from "@mui/material";
 import { debounce } from "@mui/material/utils";
 import { useAppDispatch } from "../redux-hooks";
 import { setFilter } from "../features/Filter/filterSlice";
+import { selectFilter } from "../features/Filter/filterSelectors";
+import { useSelector } from "react-redux";
 
 interface filterProps {
   onChange(): void;
@@ -11,6 +13,15 @@ interface filterProps {
 
 const Filter = ({ onChange }: filterProps) => {
   const dispatch = useAppDispatch();
+  const filterFromState = useSelector(selectFilter);
+
+  const filterInput = document.querySelector("#filter") as HTMLInputElement;
+
+  useEffect(() => {
+    if (filterInput) {
+      filterInput.value = filterFromState;
+    }
+  }, [filterInput, filterFromState]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(setFilter(event.target.value.trim()));
@@ -19,6 +30,7 @@ const Filter = ({ onChange }: filterProps) => {
   return (
     <>
       <TextField
+        id="filter"
         onChange={debounce(handleChange, 500)}
         label="Filter news"
         autoComplete="off"
